7b2e242815a78bbae333055643f42a139dc99d95
Feranmi Ayoola <bloggerlucky43@gmail.com>
2025-04-17 13:18:43 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
-    host: '0.0.0.0',  // Ensure your app is publicly accessible
-    port: process.env.PORT || 4173,  // Use Render's port or fallback to 4173
-    historyApiFallback: true,  // for handling SPA routing
-  }
+    historyApiFallback: true, // for handling SPA routing
+  },
+  preview: {
+    allowedHosts: ['divinemyst.onrender.com'], // Add your Render host here
+  },
 })
